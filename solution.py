d2f5d6e22d5f766c7a43b99cd7be77fa40c071fe
Charles Bos <charlesbos1@gmail.com>
2017-12-10 13:59:15 +0000

    changed activation function to tanh, added another hidden layer, changed number of epochs, changed dataset file to train_small.csv

--- solution.py
+++ solution.py
@@ -9,20 +9,27 @@ numpy.random.seed(7)
 
 class Solution :
     def __init__(self) :
-        traindata = numpy.loadtxt("train2.csv", delimiter = ",")
-        X = traindata[:,0:8]
-        Y = traindata[:,8]
+        traindata = numpy.loadtxt("train_small.csv", delimiter = ",")
+        X = traindata[:,0:15]
+        Y = traindata[:,15]
 
         # Create model
         model = Sequential()
-        model.add(Dense(12, input_dim=8, activation='relu'))
-        model.add(Dense(8, activation='relu'))
+        model.add(Dense(500, input_dim=15, activation='tanh'))
+        model.add(Dense(250, activation='tanh'))
+        model.add(Dense(100, activation='tanh'))
         model.add(Dense(1, activation='relu'))
 
         # Compile model
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.compile(loss='mean_squared_error', optimizer='adam')
 
         # Run model
-        model.fit(X, Y, epochs = 150, batch_size=10)
+        model.fit(X, Y, epochs = 2000, batch_size=50)
+
+        # Predict
+        predictions = model.predict(X)
+
+        for x in range(0, len(predictions)) :
+            print(str(predictions[x]) + " : " + str(Y[x]))
 
 Solution()
