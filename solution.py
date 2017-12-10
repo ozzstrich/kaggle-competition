24d0f17aeefaea59094af5d714aab50f00ab94d2
Charles Bos <charlesbos1@gmail.com>
2017-12-10 16:04:33 +0000

    Pull all code out of a class. we don't need classes for such a small program

--- solution.py
+++ solution.py
@@ -8,51 +8,47 @@ from keras.models import Sequential
 from keras.layers import Dense
 numpy.random.seed(14)
 
-class Solution :
-    def __init__(self) :
-        epochs = 1000 
-        
-        traindata = numpy.loadtxt("train_small.csv", delimiter = ",")
-        X = traindata[:,0:15]
-        Y = traindata[:,15]
+epochs = 1000 
 
-        # Create model
-        model = Sequential()
-        model.add(Dense(500, input_dim=15, activation='tanh'))
-        model.add(Dense(250, activation='tanh'))
-        model.add(Dense(100, activation='tanh'))
-        model.add(Dense(1, activation='relu'))
+traindata = numpy.loadtxt("train_small.csv", delimiter = ",")
+X = traindata[:,0:15]
+Y = traindata[:,15]
 
-        # Compile model
-        model.compile(loss='mean_squared_error', optimizer='adam')
+# Create model
+model = Sequential()
+model.add(Dense(500, input_dim=15, activation='tanh'))
+model.add(Dense(250, activation='tanh'))
+model.add(Dense(100, activation='tanh'))
+model.add(Dense(1, activation='relu'))
 
-        # Run model
-        model.fit(X, Y, epochs = epochs, batch_size=50)
+# Compile model
+model.compile(loss='mean_squared_error', optimizer='adam')
 
-        # Predict
-        predictions = model.predict(X)
+# Run model
+model.fit(X, Y, epochs = epochs, batch_size=50)
 
-        rmse = 0
-        for x in range(0, len(predictions)) :
-            #print(str(predictions[x]) + " : " + str(Y[x]))
-            if Y[x] == 0 : continue
-            rmse += math.pow(((Y[x] - predictions[x]) / Y[x]), 2)
-        rmse /= len(Y)
-        rmse = math.sqrt(rmse)
-        print("\nTrain RMSE:", rmse)
+# Predict
+predictions = model.predict(X)
 
-        testdata = numpy.loadtxt("test_small1.csv", delimiter = ",")
-        Xt = testdata[:,0:15]
-        Yt = testdata[:,15]
-        predictionsT = model.predict(Xt)
-        
-        rmse = 0
-        for x in range(0, len(predictionsT)) :
-            #print(str(predictions[x]) + " : " + str(Y[x]))
-            if Yt[x] == 0 : continue
-            rmse += math.pow(((Yt[x] - predictionsT[x]) / Yt[x]), 2)
-        rmse /= len(Yt)
-        rmse = math.sqrt(rmse)
-        print("\nTest RMSE:", rmse)
+rmse = 0
+for x in range(0, len(predictions)) :
+    #print(str(predictions[x]) + " : " + str(Y[x]))
+    if Y[x] == 0 : continue
+    rmse += math.pow(((Y[x] - predictions[x]) / Y[x]), 2)
+rmse /= len(Y)
+rmse = math.sqrt(rmse)
+print("\nTrain RMSE:", rmse)
 
-Solution()
+testdata = numpy.loadtxt("test_small1.csv", delimiter = ",")
+Xt = testdata[:,0:15]
+Yt = testdata[:,15]
+predictionsT = model.predict(Xt)
+
+rmse = 0
+for x in range(0, len(predictionsT)) :
+    #print(str(predictions[x]) + " : " + str(Y[x]))
+    if Yt[x] == 0 : continue
+    rmse += math.pow(((Yt[x] - predictionsT[x]) / Yt[x]), 2)
+rmse /= len(Yt)
+rmse = math.sqrt(rmse)
+print("\nTest RMSE:", rmse)
